41b44cc67c88c8a857cc2e16beb9c620e2b084fc
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 00:13:19 -0400

    Updated ch5-self-organizing-map.py.

--- maning_shukla/ch5-self-organizing-map.py
+++ maning_shukla/ch5-self-organizing-map.py
@@ -32,4 +32,5 @@ class SOM:
         rate = tf.multiply(alpha, neigh_factor)
         rate_factor = tf.stack([tf.tile(tf.slice(rate, [i], [1]),
                                 [self.dim]) for i in range(num_nodes)])
-        nodes_diff = tf.multiply(rate_factor, tf.subtract(tf.stack([x for i in range(num_nodes)]), sel))
+        nodes_diff = tf.multiply(rate_factor, tf.subtract(tf.stack([x for i in range(num_nodes)]), self.nodes))
+        upda
