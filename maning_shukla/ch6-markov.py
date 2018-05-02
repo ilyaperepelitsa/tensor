6cefafd376bb016534f4428c933843b7fc0262f9
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:29:50 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -30,4 +30,5 @@ class HMM(object):
     def forward_op(self):
         transitions = tf.matmul(self.fwd, tf.transpose(self.get_emission(self.obs_idx)))
         weighted_transitions = transitions * self.trans_prob
-        fwd = tf.reduce_sum(weighted_trams)
+        fwd = tf.reduce_sum(weighted_transitions, 0)
+        
