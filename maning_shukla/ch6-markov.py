ef63715e950cb777b4f437b3839a13353168c68c
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:51:15 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -8,7 +8,7 @@ class HMM(object):
         self.initial_prob = initial_prob
         self.trans_prob = trans_prob
         self.emission = tf.constant(obs_prob)
-        self.viterbi = tf.placeholder()
+        self.viterbi = tf.placeholder(tf.float64)
 
         assert self.initial_prob.shape == (self.N, 1)
         assert self.trans_prob.shape == (self.N, self.N)
