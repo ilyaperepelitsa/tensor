8fcabfd0add49e6ab275b7b3a78b97b2a67c1f1b
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:20:24 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -5,4 +5,4 @@ import tensorflow as tf
 class HMM(object):
     def __init__(self, initial_prob, trans_prob, obs_prob):
         self.N = np.size(initial_prob)
-        self.initial_prob
+        self.initial_prob = initial_prob
