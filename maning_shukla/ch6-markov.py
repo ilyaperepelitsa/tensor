e26297286bbd7478462910241bba849d515de484
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:41:54 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -43,14 +43,14 @@ class HMM(object):
 
 
 
-states = ("Rainy", "Sunny")
-observations = ("walk", "shop", "clean")
-start_probability = {"Rainy": 0.6, "Sunny": 0.4}
-transition_probability = {
-    "Rainy": {"Rainy": 0.7, "Sunny": 0.3},
-    "Sunny": {"Rainy": 0.4, "Sunny": 0.6}
-}
-emission_probability = {
-    "Rainy": {"walk": 0.1, "shop": 0.4, "clean": 0.5},
-    "Sunny": {"walk": 0.6, "shop": 0.3, "clean": 0.1}
-}
+# states = ("Rainy", "Sunny")
+# observations = ("walk", "shop", "clean")
+# start_probability = {"Rainy": 0.6, "Sunny": 0.4}
+# transition_probability = {
+#     "Rainy": {"Rainy": 0.7, "Sunny": 0.3},
+#     "Sunny": {"Rainy": 0.4, "Sunny": 0.6}
+# }
+# emission_probability = {
+#     "Rainy": {"walk": 0.1, "shop": 0.4, "clean": 0.5},
+#     "Sunny": {"walk": 0.6, "shop": 0.3, "clean": 0.1}
+# }
