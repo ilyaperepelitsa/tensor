68c7fb66be8a7730dbb09d2233686485accbebd7
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:38:50 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -46,4 +46,4 @@ class HMM(object):
 states = ("Rainy", "Sunny")
 observations = ("walk", "shop", "clean")
 start_probability = {"Rainy": 0.6, "Sunny": 0.4}
-transition_probabilu
+transition_probability = {}
