151abda1c7735d2ff972ee0fbb218f2cf4c26875
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:44:28 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -59,4 +59,4 @@ initial_prob = np.array([[0.6], [0.4]])
 trans_prob = np.array([[0.7, 0.3], [0.4, 0.6]])
 obs_prob = np.array([[0.1, 0.4, 0.5],
                      [0.6, 0.3, 0.1]])
-hmm = HMM(initial_prob = initial_prob, trans_prob = trans_prob, obs_prob = ob)
+hmm = HMM(initial_prob = initial_prob, trans_prob = trans_prob, obs_prob = obs_prob)
