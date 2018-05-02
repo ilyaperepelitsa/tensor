7cf4ba0964d5b54889cd62068e44120b84370398
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:59:20 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -57,7 +57,7 @@ def viterbi_decode(sess, hmm, observations):
     backpts = np.ones((hmm.N, len(observations)), "int32") * -1
     for t in range(1, len(observations)):
         viterbi, backpt = sess.run([hmm.decode_op(), hmm.backpt_op()],
-                            feed_dict = {hmm.obs: })
+                            feed_dict = {hmm.obs: observations[t]})
 
 # states = ("Rainy", "Sunny")
 # observations = ("walk", "shop", "clean")
