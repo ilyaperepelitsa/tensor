13b846c750042d3d3cec5c7cf62249fe76dc3e98
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:54:54 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -40,7 +40,8 @@ class HMM(object):
         return tf.reshape(viterbi, tf.shape(self.viterbi))
 
     def backpt_op(self):
-        back_transitions = tf.matmul(self.viterbi, np.ones((1, s)))
+        back_transitions = tf.matmul(self.viterbi, np.ones((1, self.N)))
+        
 
 def forward_algoritm(sess, hmm, observations):
     fwd = sess.run(hmm.forward_init_op(), feed_dict = {hmm.obs_idx: observations[0]})
