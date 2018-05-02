19bd4e0c2d08301bb2d1e1f6cd6f068bdd3ee795
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:20:33 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -6,4 +6,4 @@ class HMM(object):
     def __init__(self, initial_prob, trans_prob, obs_prob):
         self.N = np.size(initial_prob)
         self.initial_prob = initial_prob
-        self.trans
+        self.trans_prob = trans
