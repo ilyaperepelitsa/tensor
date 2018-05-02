5ff3308325e5ee946ddaa4c6f474706038879d2d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:26:27 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -24,4 +24,4 @@ class HMM(object):
 
     def forward_init_op(self):
         obs_prob = self.get_emission(self.obs_idx)
-        fwd = tf.multiply(self)
+        fwd = tf.multiply(self.initial_prob, )
