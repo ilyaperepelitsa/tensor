fc3e2521ddea1afb68b887813c9341a39d516a3d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:52:43 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -36,7 +36,7 @@ class HMM(object):
 
     def decode_op(self):
         transitions = tf.matmul(self.viterbi, tf.transpose(self.get_emissions * self.trans_prob))
-        viterbi - 
+        viterbi = tf.reduce_m
 
 def forward_algoritm(sess, hmm, observations):
     fwd = sess.run(hmm.forward_init_op(), feed_dict = {hmm.obs_idx: observations[0]})
