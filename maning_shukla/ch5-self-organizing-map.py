1c77c214df1532a5a4cbac58f1b1a002b9a0b3c2
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-01 23:14:22 -0400

    Updated ch5-self-organizing-map.py.

--- maning_shukla/ch5-self-organizing-map.py
+++ maning_shukla/ch5-self-organizing-map.py
@@ -23,4 +23,5 @@ class SOM:
 
     def get_propagation(self, bmu_loc, x, iter):
         num_nodes = self.width * self.height
-        rate = 1.0 - tf.div(iter, self.num)
+        rate = 1.0 - tf.div(iter, self.num_iters)
+        alpha 
