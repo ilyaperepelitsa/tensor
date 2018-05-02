5ff2ec1690d5c7efa385a5f61c2ed8a460b48e56
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 03:00:41 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -60,7 +60,7 @@ def viterbi_decode(sess, hmm, observations):
                             feed_dict = {hmm.obs: observations[t],
                                         hmm.viterbi : viterbi})
         backpts[:, t] = backpt
-    tokens = [viterbi[:, -1]]
+    tokens = [viterbi[:, -1].ar]
 
 # states = ("Rainy", "Sunny")
 # observations = ("walk", "shop", "clean")
