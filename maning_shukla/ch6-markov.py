2b40e4fcf6b69db6c6decf052eee8019fe92b39e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:56:47 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -53,7 +53,7 @@ def forward_algoritm(sess, hmm, observations):
     return prob
 
 def viterbi_decode(sess, hmm, observations):
-    viterbi = sess.run(hmm.forward_init_op(), feed_dict = {})
+    viterbi = sess.run(hmm.forward_init_op(), feed_dict = {hmm.lo})
 
 # states = ("Rainy", "Sunny")
 # observations = ("walk", "shop", "clean")
