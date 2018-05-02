21ffab2573fd587041be6dea2fe2ae0073a45ace
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 02:50:59 -0400

    Updated ch6-markov.py.

--- maning_shukla/ch6-markov.py
+++ maning_shukla/ch6-markov.py
@@ -8,6 +8,7 @@ class HMM(object):
         self.initial_prob = initial_prob
         self.trans_prob = trans_prob
         self.emission = tf.constant(obs_prob)
+        self.
 
         assert self.initial_prob.shape == (self.N, 1)
         assert self.trans_prob.shape == (self.N, self.N)
@@ -55,12 +56,12 @@ def forward_algoritm(sess, hmm, observations):
 #     "Sunny": {"walk": 0.6, "shop": 0.3, "clean": 0.1}
 # }
 # if __name__ == "__main__":
-    initial_prob = np.array([[0.6], [0.4]])
-    trans_prob = np.array([[0.7, 0.3], [0.4, 0.6]])
-    obs_prob = np.array([[0.1, 0.4, 0.5],
-                         [0.6, 0.3, 0.1]])
-    hmm = HMM(initial_prob = initial_prob, trans_prob = trans_prob, obs_prob = obs_prob)
-    observations = [0, 1, 1, 2, 1]
-    with tf.Session() as sess:
-        prob = forward_algoritm(sess, hmm, observations)
-        print("Probability of observing {} is {}".format(observations, prob))
+initial_prob = np.array([[0.6], [0.4]])
+trans_prob = np.array([[0.7, 0.3], [0.4, 0.6]])
+obs_prob = np.array([[0.1, 0.4, 0.5],
+                     [0.6, 0.3, 0.1]])
+hmm = HMM(initial_prob = initial_prob, trans_prob = trans_prob, obs_prob = obs_prob)
+observations = [0, 1, 1, 2, 1]
+with tf.Session() as sess:
+    prob = forward_algoritm(sess, hmm, observations)
+    print("Probability of observing {} is {}".format(observations, prob))
