ea88a7a74424fae591899aadcbc5caf35bd80491
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-02 01:02:13 -0400

    Updated ch5-self-organizing-map.py.

--- maning_shukla/ch5-self-organizing-map.py
+++ maning_shukla/ch5-self-organizing-map.py
@@ -59,4 +59,4 @@ class SOM:
                                                                 self.iter: i})
             centroid_grid = [[] for i in range(self.width)]
             self.nodes_val = list(sess.run(self.nodes))
-            self.locs_val = list(sess.run(self.))
+            self.locs_val = list(sess.run(self.node_locs))
