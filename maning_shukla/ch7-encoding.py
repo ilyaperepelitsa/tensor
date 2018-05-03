518517a4e24fccbdac8f07b9ae9f2d8d1a381abe
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 01:39:14 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -33,7 +33,7 @@ class Autoencoder:
         a = np.random.choice(len(X), size, replace = False)
         return X[a]
 
-    def train(self, data):
+    def train(self, data, batc):
         num_samples = len(data)
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
