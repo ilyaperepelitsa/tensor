6e9c0caac6c5fce60fc05acd8215dcf60af619e9
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 01:26:14 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -29,7 +29,8 @@ class Autoencoder:
         self.train_op = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
         self.saver = tf.train.Saver()
 
-    def get_batch(X, size)
+    def get_batch(X, size):
+        a
 
     def train(self, data):
         num_samples = len(data)
