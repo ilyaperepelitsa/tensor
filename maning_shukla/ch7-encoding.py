935edad62b740982817eaf7d12282b1e78795758
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 00:24:44 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -5,7 +5,7 @@ import numpy as np
 class Autoencoder:
     def __init__(self, input_dim, hidden_dim, epoch = 250, learning_rate = 0.001):
         self.epoch = epoch
-        self.lear
+        self.learning_rate = learning_rate
     def train(self, data):
 
     def test(self, data):
