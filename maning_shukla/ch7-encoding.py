6315f5aeea46dae69d1f13dc2f0e8f98e13a0db2
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 01:42:32 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -66,7 +66,7 @@ hidden_dim = 1
 data = datasets.load_iris().data
 input_dim = len(data[0])
 ae = Autoencoder(input_dim, hidden_dim)
-ae.train(data)
+ae.train(data, batc)
 ae.test([[8, 4, 6, 2]])
 
 
