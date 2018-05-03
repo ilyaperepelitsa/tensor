ef692f57b7079e6a8127652a8ff4e6b151f17f8c
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 02:54:39 -0400

    Updated ch8-yahoo_finance.py.

--- maning_shukla/ch8-yahoo_finance.py
+++ maning_shukla/ch8-yahoo_finance.py
@@ -7,4 +7,4 @@ import random
 
 def get_prices(share_symbol, start_date, end_date, cache_filename = "stock_prices.npy"):
     try:
-        stock_prices = np.load
+        stock_prices = np.load(cache_)
