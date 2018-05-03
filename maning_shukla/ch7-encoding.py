d3d175ab26774259d27a5963f203bf1c95ef098e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 01:09:50 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -47,3 +47,4 @@ class Autoencoder:
             self.saver.restore(sess, "./model.ckpt")
             hidden, recontructed = sess.run([self.encoded, self.decoded],
             feed_dict = {self.x: data})
+        
