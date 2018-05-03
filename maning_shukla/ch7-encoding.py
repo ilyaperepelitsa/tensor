588bdbe7257b8ceae3a919f839d24e2578c69f3e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 01:05:37 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -40,5 +40,5 @@ class Autoencoder:
                 if i % 10 == 0:
                     print("epoch {0}: loss = {1}".format(i, l))
                     self.saver.save(sess, "./model.ckpt")
-            self
+            self.saver.save()
     def test(self, data):
