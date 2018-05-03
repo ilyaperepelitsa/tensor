9121889116d3cc91d72c822fc064d0f0510bb5e8
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 02:56:20 -0400

    Updated ch8-yahoo_finance.py.

--- maning_shukla/ch8-yahoo_finance.py
+++ maning_shukla/ch8-yahoo_finance.py
@@ -13,4 +13,4 @@ def get_prices(share_symbol, start_date, end_date, cache_filename = "stock_price
         stock_hist = share.get_historical(start_date, end_date)
         stock_prices = [stock_price["Open"] for stock_price in stock_hist]
         np.save(cache_filename, stok_prices)
-    retur
+    return stock_price
