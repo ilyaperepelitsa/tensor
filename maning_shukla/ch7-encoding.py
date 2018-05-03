bc1cd0763d48397596b979f5765ba41e862c6cdd
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 00:49:59 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -26,7 +26,7 @@ class Autoencoder:
         self.decoded = decoded
 
         self.loss = tf.sqrt(tf.reduce_mean(tf.square(tf.subtract(self.x, self.decoded))))
-        self.train_op = tf.train.
+        self.train_op = tf.train.RMSPropOptimizer
 
     def train(self, data):
 
