54def9f6c3de1de8bc8a09bee8da28e7e0355dc3
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 01:02:13 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -35,5 +35,5 @@ class Autoencoder:
             sess.run(tf.global_variables_initializer())
             for i in range(self.epoch):
                 for j in range(num_samples):
-                    l, _ = sess.run()
+                    l, _ = sess.run([self])
     def test(self, data):
