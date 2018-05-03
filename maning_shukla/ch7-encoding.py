d89ada793c09b64ae8fbb0e752c1a12cec8fc213
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 00:43:46 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -17,7 +17,7 @@ class Autoencoder:
 
         with tf.name_scope("decode"):
             weights = tf.Variable(tf.random_normal([hidden_dim, input_dim],
-                            dtype = tf.float32))
+                            dtype = tf.float32), )
 
     def train(self, data):
 
