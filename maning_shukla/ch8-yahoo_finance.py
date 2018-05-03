15e1d210775e42cc335207d426b97c4d7d1f744d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 02:54:23 -0400

    Updated ch8-yahoo_finance.py.

--- maning_shukla/ch8-yahoo_finance.py
+++ maning_shukla/ch8-yahoo_finance.py
@@ -5,4 +5,4 @@ import tensorflow as tf
 import random
 
 
-def get_prices(share_symbol, start_date, end_date, cache_filename = "stock_"):
+def get_prices(share_symbol, start_date, end_date, cache_filename = "stock_prices.npy"):
