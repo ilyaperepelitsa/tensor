6536aace03dcb7fe0b07c3c699c7a66fbbf0ef41
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-03 01:34:34 -0400

    Updated ch7-encoding.py.

--- maning_shukla/ch7-encoding.py
+++ maning_shukla/ch7-encoding.py
@@ -43,6 +43,8 @@ class Autoencoder:
                     batch_data = get_batch(data, self.batch_size)
                     l, _ = sess.run([self.loss, self.train_op],
                     feed_dict = {self.x: [data[j]]})
+                    l, _ = sess.run([self.loss, self.train_op],
+                    feed_dict = {self.x: batch_data})
                 if i % 10 == 0:
                     print("epoch {0}: loss = {1}".format(i, l))
                     self.saver.save(sess, "./model.ckpt")
