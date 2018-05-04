6e077d7f33f1c3be22abadc9c66b49d909ec65d9
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 11:24:31 -0400

    Updated ch9-cnn-real.py.

--- maning_shukla/ch9-cnn-real.py
+++ maning_shukla/ch9-cnn-real.py
@@ -50,4 +50,9 @@ b_out = tf.Variable(tf.random_normal([1024]))
 
 def conv_layer(conv, k = 2):
     return tf.nn.max_pool(conv, ksize = [1, k, k, 1], strides = [1, k, k, 1],
-                            padding = )
+                            padding = "SAME")
+
+
+
+def model():
+    
