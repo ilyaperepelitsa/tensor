e7790011283288f022c5ef2ac8a4b83d6d323768
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 02:49:43 -0400

    Updated ch9-cnn-cifar.py.

--- maning_shukla/ch9-cnn-cifar.py
+++ maning_shukla/ch9-cnn-cifar.py
@@ -121,4 +121,4 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     conv_val = sess.run(conv)
-    show_conv_results(conv_val, "")
+    show_conv_results(conv_val, "stepl_conb")
