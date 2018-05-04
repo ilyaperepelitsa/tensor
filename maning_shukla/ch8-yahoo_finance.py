f2455e706ff1d21d4592ed0dda47e59bfc3abd88
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 01:19:25 -0400

    Updated ch8-yahoo_finance.py.

--- maning_shukla/ch8-yahoo_finance.py
+++ maning_shukla/ch8-yahoo_finance.py
@@ -79,4 +79,5 @@ class RandomDecisionPolicy(DecisionPolicy):
             current_state = np.asmatrix(np.hstack((prices[i:i+hist], budget, num_stocks)))
             current_portfolio = budget + num_stocks * share_value
             action = policy.select_action(current_state, i)
-            share_value = float(prices[i+hi])
+            share_value = float(prices[i+hist])
+            if 
