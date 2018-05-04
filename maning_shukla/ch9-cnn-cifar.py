a36073298b720b9b8208ca6e79459fde32c49488
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 03:01:33 -0400

    Updated ch9-cnn-cifar.py.

--- maning_shukla/ch9-cnn-cifar.py
+++ maning_shukla/ch9-cnn-cifar.py
@@ -134,4 +134,4 @@ maxpool = tf.nn.max_pool(conv_out, ksize = [1, k ,k, 1],
                             strides = [1, k, k, 1], padding = "SAME")
 with tf.Session() as sess:
     maxpool_val = sess.run(maxpool)
-    show_conv_results(maxpool_val, "step3")
+    show_conv_results(maxpool_val, "step3_maxpoo")
