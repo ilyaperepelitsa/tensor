a3efc5b299439d42f4462c7dd3d79a918a00a601
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 02:46:37 -0400

    Updated ch9-cnn-cifar.py.

--- maning_shukla/ch9-cnn-cifar.py
+++ maning_shukla/ch9-cnn-cifar.py
@@ -113,4 +113,4 @@ plt.savefig("input_image.png")
 X = tf.reshape(raw_data, shape = [-1, 24, 24, 1])
 
 b = tf.Variable(tf.random_normal([32]))
-conv = tf.nn.conv2d(x, W, strides = [1, 1, 1, 1])
+conv = tf.nn.conv2d(x, W, strides = [1, 1, 1, 1], padding = )
