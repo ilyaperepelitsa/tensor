bd264320f5c38ec7d6d24dc9dd304f49634d44c8
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 02:34:43 -0400

    Updated ch9-cnn-cifar.py.

--- maning_shukla/ch9-cnn-cifar.py
+++ maning_shukla/ch9-cnn-cifar.py
@@ -76,4 +76,5 @@ def show_weights(W, filename = None):
     plt.figure()
     rows, cols = 4, 8
     for i in range(np.shape(W)[3]):
-        img = W[:, :, 0, ]
+        img = W[:, :, 0, i]
+        
