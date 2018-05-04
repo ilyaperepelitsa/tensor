88247191ba571a006eea2cf89a129b473aaad106
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 11:35:16 -0400

    Updated ch9-cnn-real.py.

--- maning_shukla/ch9-cnn-real.py
+++ maning_shukla/ch9-cnn-real.py
@@ -77,3 +77,4 @@ model_op = model()
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = model_op, labels = y))
 train_op = tf.train.AdamOptimizer(learning_rate = 0.001).minimize(cost)
 correct_pred = tf.equal(tf.argmax(model_op, 1), tf.argmax(y, 1))
+accuracy = tf.r
