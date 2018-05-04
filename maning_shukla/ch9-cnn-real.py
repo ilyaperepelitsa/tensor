dc27ee2a1c8f2659c824b4f0d5b99bb37ed9a87b
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 11:27:35 -0400

    Updated ch9-cnn-real.py.

--- maning_shukla/ch9-cnn-real.py
+++ maning_shukla/ch9-cnn-real.py
@@ -62,5 +62,5 @@ def model():
     norm1 = tf.nn.lrn(maxpool_out1, 4, bias = 1.0, alpha = 0.001 / 9.0, beta = 0.75)
 
     conv_out2 = conv_layer(x_reshaped, W2, b2)
-    norm2 = tf.nn.lrn(maxpool_out1, 4, bias = 1.0, alpha = 0.001 / 9.0, beta = 0.75)
+    norm2 = tf.nn.lrn(conv_out2, 4, bias = 1.0, alpha = 0.001 / 9.0, beta = 0.75)
     maxpool_out1 = maxpool_layer(conv_out)
