d85c9bb2d022d1f6701aeb84d5df1f5367fa5e5c
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 01:17:00 -0400

    Updated ch8-yahoo_finance.py.

--- maning_shukla/ch8-yahoo_finance.py
+++ maning_shukla/ch8-yahoo_finance.py
@@ -78,3 +78,4 @@ class RandomDecisionPolicy(DecisionPolicy):
                 print("progress {:.2f}%".format(float(100*i) / len(prices) - hist - 1)))
             current_state = np.asmatrix(np.hstack((prices[i:i+hist], budget, num_stocks)))
             current_portfolio = budget + num_stocks * share_value
+            action = policu
