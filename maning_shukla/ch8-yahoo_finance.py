92535183224f3cf8b5f37023e27fc1d8d7416d14
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 01:44:14 -0400

    Updated ch8-yahoo_finance.py.

--- maning_shukla/ch8-yahoo_finance.py
+++ maning_shukla/ch8-yahoo_finance.py
@@ -78,7 +78,6 @@ def run_simulation(policy, initial_budget, initial_num_stocks, prices, hist):
             print('progress {:.2f}%'.format(float(100*i) / (len(prices) - hist - 1)))
         current_state = np.asmatrix(np.hstack((prices[i:i+hist], budget, num_stocks)))
         current_portfolio = budget + num_stocks * share_value
-        print(current_state, i)
         action = policy.select_action(current_state, i)
         share_value = float(prices[i + hist + 1])
         if action == 'Buy' and budget >= share_value:
