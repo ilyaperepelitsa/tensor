6e74cc0c2f98586a38fe77c6f3b756f680671f90
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 01:01:23 -0400

    Updated ch8-yahoo_finance.py.

--- maning_shukla/ch8-yahoo_finance.py
+++ maning_shukla/ch8-yahoo_finance.py
@@ -13,35 +13,35 @@ end = datetime(2017, 5, 24)
 
 df = get_historical_data("AAPL", start=start, end=end, output_format='pandas')
 df.head()
-list(df.high)
+list(df.close)
 
-def get_prices(share_symbol, start_date, end_date, cache_filename = "/Users/ilyaperepelitsa/Downloads/stock_prices.npy"):
-    try:
-        stock_prices = np.load(cache_filename)
-    except IOError:
-        share = Share(share_symbol)
-        stock_hist = share.get_historical(start_date, end_date)
-        stock_prices = [stock_price["Open"] for stock_price in stock_hist]
-        np.save(cache_filename, stok_prices)
-    return stock_price.astype(float)
-
-# import urllib2
-from bs4 import BeautifulSoup as bs
-
-def get_historical_data(name, number_of_days):
-    data = []
-    url = "https://finance.yahoo.com/quote/" + name + "/history/"
-    rows = bs(urllib2.urlopen(url).read()).findAll('table')[0].tbody.findAll('tr')
-
-    for each_row in rows:
-        divs = each_row.findAll('td')
-        if divs[1].span.text  != 'Dividend': #Ignore this row in the table
-            #I'm only interested in 'Open' price; For other values, play with divs[1 - 5]
-            data.append({'Date': divs[0].span.text, 'Open': float(divs[1].span.text.replace(',',''))})
-
-    return data[:number_of_days]
-
-get_historical_data("AAPL", 200)
+# def get_prices(share_symbol, start_date, end_date, cache_filename = "/Users/ilyaperepelitsa/Downloads/stock_prices.npy"):
+#     try:
+#         stock_prices = np.load(cache_filename)
+#     except IOError:
+#         share = Share(share_symbol)
+#         stock_hist = share.get_historical(start_date, end_date)
+#         stock_prices = [stock_price["Open"] for stock_price in stock_hist]
+#         np.save(cache_filename, stok_prices)
+#     return stock_price.astype(float)
+#
+# # import urllib2
+# from bs4 import BeautifulSoup as bs
+#
+# def get_historical_data(name, number_of_days):
+#     data = []
+#     url = "https://finance.yahoo.com/quote/" + name + "/history/"
+#     rows = bs(urllib2.urlopen(url).read()).findAll('table')[0].tbody.findAll('tr')
+#
+#     for each_row in rows:
+#         divs = each_row.findAll('td')
+#         if divs[1].span.text  != 'Dividend': #Ignore this row in the table
+#             #I'm only interested in 'Open' price; For other values, play with divs[1 - 5]
+#             data.append({'Date': divs[0].span.text, 'Open': float(divs[1].span.text.replace(',',''))})
+#
+#     return data[:number_of_days]
+#
+# get_historical_data("AAPL", 200)
 def plot_prices(prices):
     plt.title("Opening stock prices")
     plt.xlabel("day")
