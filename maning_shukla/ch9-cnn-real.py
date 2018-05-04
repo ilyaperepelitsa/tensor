b40b0bffed2d80b417473f2300db3363e970c3cf
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 03:09:10 -0400

    Updated ch9-cnn-real.py.

--- maning_shukla/ch9-cnn-real.py
+++ maning_shukla/ch9-cnn-real.py
@@ -2,5 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+def unpickle(file):
+    fo = open(file, "rb")
+    dict = pickle.load(fo, encoding = "latin1")
+    fo.close()
+    return dict
 
-names, data, labels
+names, data, labels =
