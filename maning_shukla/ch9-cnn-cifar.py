8356542f600ce7d6b6b8e08e7e099c4ba1c77591
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 02:05:36 -0400

    Updated ch9-cnn-cifar.py.

--- maning_shukla/ch9-cnn-cifar.py
+++ maning_shukla/ch9-cnn-cifar.py
@@ -18,4 +18,4 @@ def clean(data):
     meansT = means.reshape(len(means), 1)
     stds = np.std(img_data, axis = 1)
     stdsT = stds.reshape(len(stds), 1)
-    adj_stds = np.maximum(stdsT, 1.0 / np.)
+    adj_stds = np.maximum(stdsT, 1.0 / np.sqrt(img))
