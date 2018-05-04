5d7442dc8f3bb382daf60c93f1e15bfd18f08d88
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 02:38:29 -0400

    Updated ch9-cnn-cifar.py.

--- maning_shukla/ch9-cnn-cifar.py
+++ maning_shukla/ch9-cnn-cifar.py
@@ -89,4 +89,4 @@ with tf.Session as sess:
     sess.run(tf.global_variables_initializer())
 
     W_val = sess.run(W)
-    show_weights(W_val, "step0_wi")
+    show_weights(W_val, "step0_weights.png")
