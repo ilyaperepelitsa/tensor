2b3bbee56de9673868d52660ff5184f806db19c0
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 01:22:37 -0400

    Updated ch8-yahoo_finance.py.

--- maning_shukla/ch8-yahoo_finance.py
+++ maning_shukla/ch8-yahoo_finance.py
@@ -88,4 +88,4 @@ class RandomDecisionPolicy(DecisionPolicy):
                 num_stocks -= 1
             else:
                 action = "Hold"
-            new_portf
+            new_portfolio - b
