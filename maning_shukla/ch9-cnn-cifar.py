82b3c82fbeb4a563f39069d5aae3856f9f338b97
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 02:43:09 -0400

    Updated ch9-cnn-cifar.py.

--- maning_shukla/ch9-cnn-cifar.py
+++ maning_shukla/ch9-cnn-cifar.py
@@ -99,4 +99,5 @@ def show_conv_results(data, filename = None):
         plt.subplot(rows, cols, i + 1)
         plt.imshow(img, cmap = "Greys_r", interpolation = "none")
         plt.axis("off")
-    
+    if filename:
+        plt.sa
