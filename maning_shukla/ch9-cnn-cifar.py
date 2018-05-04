54d98f24989bd048fb681cb1cf98805839423825
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-04 02:34:30 -0400

    Updated ch9-cnn-cifar.py.

--- maning_shukla/ch9-cnn-cifar.py
+++ maning_shukla/ch9-cnn-cifar.py
@@ -75,4 +75,5 @@ W = tf.Variable(tf.random_normal([5, 5, 1, 32]))
 def show_weights(W, filename = None):
     plt.figure()
     rows, cols = 4, 8
-    for i in range(np.shape(w))
+    for i in range(np.shape(W)[3]):
+        
