f6b4e390844567df8fd33d5e9c267bc56166d207
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 22:18:41 -0400

    Updated ch10-rnn.py.

--- maning_shukla/ch10-rnn.py
+++ maning_shukla/ch10-rnn.py
@@ -46,4 +46,4 @@ class SeriesPredictor:
             print("Model saved to {}".format(save_path))
     def test(self, test_x):
         with tf.Session() as sess:
-            tf.get
+            tf.get_variable_scope()
