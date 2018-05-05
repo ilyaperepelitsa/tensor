bfca6d79356d83a92ed679cc8ffc8cdfce072b5f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-05 00:25:40 -0400

    Updated ch11-first.py.

--- maning_shukla/ch11-first.py
+++ maning_shukla/ch11-first.py
@@ -11,7 +11,7 @@ def make_cell(state_dim):
     return tf.contrib.rnn.LSTMCell(state_dim)
 
 def make_multi_cell(state_dim, num_layers):
-    cells = [make]
+    cells = [make_cell(state)]
 
 with tf.variable_scope("first_cell") as scope:
     cell = make_cell(state_dim = 10)
