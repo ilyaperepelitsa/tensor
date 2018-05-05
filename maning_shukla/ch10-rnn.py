bb85af3db175d10047d8ab0682f4937851c5708b
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 21:56:46 -0400

    Updated ch10-rnn.py.

--- maning_shukla/ch10-rnn.py
+++ maning_shukla/ch10-rnn.py
@@ -22,6 +22,6 @@ class SeriesPredictor:
     def model(self):
         """
         :param x: inputs of size [T, batch_size, input_size]
-        :param W: matrix if fully-connected out
+        :param W: matrix if fully-connected output layer w
         :param x: inputs of size [T, batch_size, input_size]
         """
