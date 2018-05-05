0a811afb3947ba0d23064bb5c5b6120e873eca1e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 23:37:17 -0400

    Updated ch10-flights.py.

--- maning_shukla/ch10-flights.py
+++ maning_shukla/ch10-flights.py
@@ -93,7 +93,7 @@ train_data, actual_vals = split_data(data)
 train_x, train_y = [], []
 for i in range(len(train_data) - seq_size - 1) :
     train_x.append(np.expand_dims(train_data[i:i+seq_size], axis = 1).tolist())
-    text_y.append(train_data[i+1:i+seq_size+1])
+    train_y.append(train_data[i+1:i+seq_size+1])
 
 test_x, test_y = [], []
 for i in range(len(actual_vals) - seq_size - 1):
