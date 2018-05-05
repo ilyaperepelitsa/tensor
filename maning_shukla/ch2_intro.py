6b4bf93053bb0150aa12b6b06ae5aff0a2b9c418
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 03:38:30 -0400

    Updated ch2_intro.py.

--- maning_shukla/ch2_intro.py
+++ maning_shukla/ch2_intro.py
@@ -29,4 +29,4 @@ with tf.Session() as sess:
 
     ans = sess.run(accuracy, feed_dict = {x: data.test.images, y_true: data.test.labels})
 
-    print("Accuracy: {:.5}%".format(ans*100))
+print("Accuracy: {:.5}%".format(ans*100))
