96d3a78b04d13fa5fac4f54ce7ae7a3a09a7d422
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 22:00:39 -0400

    Updated ch10-rnn.py.

--- maning_shukla/ch10-rnn.py
+++ maning_shukla/ch10-rnn.py
@@ -26,4 +26,5 @@ class SeriesPredictor:
         :param b: vector of fully-connected output layer biases
         """
         cell = rnn.BasicLSTMCell(self.hidden_dim)
-        outputs, states = tf.nn.dynamic_rnn(cell, self.x, dtype =)
+        outputs, states = tf.nn.dynamic_rnn(cell, self.x, dtype = tf.float32)
+        
