7a239f30fcca1cea52ee455a10a6712656736d75
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 22:24:50 -0400

    Updated ch10-rnn.py.

--- maning_shukla/ch10-rnn.py
+++ maning_shukla/ch10-rnn.py
@@ -56,4 +56,4 @@ class SeriesPredictor:
 predictor = SeriesPredictor(input_dim = 1, seq_size = 4, hidden_dim = 10)
 train_x = [[[1], [2], [5], [6]],
             [[5], [7], [7], [8]],
-            [[3], [4], [5], [7]],]
+            [[3], [4], [5], [7]]]
