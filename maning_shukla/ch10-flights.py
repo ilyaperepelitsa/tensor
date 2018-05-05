7d32c028f4044eef03c0a34df984b9a80b29266e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 23:20:10 -0400

    Updated ch10-flights.py.

--- maning_shukla/ch10-flights.py
+++ maning_shukla/ch10-flights.py
@@ -82,4 +82,4 @@ train_data, actual_vals = data_loader.split_data(data)
 
 train_x. train_y = [], []
 for i in range(len(train_data) - seq_size - 1) :
-    train_x.append(np.e)
+    train_x.append(np.expand_dims(train_data))
