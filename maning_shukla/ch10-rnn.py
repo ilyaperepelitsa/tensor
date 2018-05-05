9987b6bddfac16dc19188922d86a34c3907cf6c1
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 22:08:10 -0400

    Updated ch10-rnn.py.

--- maning_shukla/ch10-rnn.py
+++ maning_shukla/ch10-rnn.py
@@ -43,4 +43,4 @@ class SeriesPredictor:
                 if i % 100 == 0:
                     print(i, mse)
             save_path = self.saver.save(sess, "model.ckpt")
-            print("Model saved to {}.")
+            print("Model saved to {}".format())
