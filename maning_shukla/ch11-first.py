b043b94ead2f49fbedcb6c2cf752d32a4120b32a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 01:38:51 -0400

    Updated ch11-first.py.

--- maning_shukla/ch11-first.py
+++ maning_shukla/ch11-first.py
@@ -127,4 +127,4 @@ encoder_output, encoder_state = tf.nn.dynamic_rnn(
 del(encoder_output)
 
 decoder_raw_seq = decoder_output_seq[:, :-1]
-go_prefixes = tf.fill([BATCH_SIZE, 1], output_)
+go_prefixes = tf.fill([BATCH_SIZE, 1], output_symbol_to)
