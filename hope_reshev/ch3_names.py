05e7a7151083b128a656844e4d9e8e35d3027341
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 15:22:36 -0400

    Updated ch3_names.py.

--- hope_reshev/ch3_names.py
+++ hope_reshev/ch3_names.py
@@ -3,4 +3,4 @@ import tensorflow as tf
 
 with tf.Graph().as_default():
     c1 = tf.constant(4, dtype = tf.float64, name = "c")
-    c2 = tf.constant(4, dtype = )
+    c2 = tf.constant(4, dtype = tf.int32, name = "c")
