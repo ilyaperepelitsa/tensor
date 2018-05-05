53d6c823284288eac1f29a9c2469feafd3b2ce8d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 22:36:52 -0400

    Updated ch10-flights.py.

--- maning_shukla/ch10-flights.py
+++ maning_shukla/ch10-flights.py
@@ -11,4 +11,5 @@ def load_series(filename, series_idx = 1):
 
             normalized_data = (data - np.mean(data)) / np.std(data)
 
-        return normalized
+        return normalized_data
+    except IO
