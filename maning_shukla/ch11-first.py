bb9f0492c59620482e3d1164dfb1f675a3f600c1
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 01:31:57 -0400

    Updated ch11-first.py.

--- maning_shukla/ch11-first.py
+++ maning_shukla/ch11-first.py
@@ -118,4 +118,4 @@ encoder_input_embedded = tf.contrib.layers.embed_sequence(
 #Encoder output
 encoder_multi_cell = make_multi_cell(RNN_STATE_DIM, RNN_NUM_LAYERS)
 
-encoder_output, encoder
+encoder_output, encoder_state = tf.nn.dynamic_rnn
