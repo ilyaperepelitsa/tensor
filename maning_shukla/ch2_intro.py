61cf171f805112247b58c61df5766c858c11d70e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 03:36:20 -0400

    Updated ch2_intro.py.

--- maning_shukla/ch2_intro.py
+++ maning_shukla/ch2_intro.py
@@ -26,3 +26,5 @@ with tf.Session() as sess:
     for _ in range(NUM_STEPS):
         batch_xs, batch_ys = data.train.next_batch(MINIBATCH_SIZE)
         sess.run(gd_step, feed_dict = {x: batch_xs, y_true: batch_ys})
+
+    ans = 
