43eefbb69b6e23ccaee73488f3f2c559a6790595
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 22:37:28 -0400

    Updated ch10-flights.py.

--- maning_shukla/ch10-flights.py
+++ maning_shukla/ch10-flights.py
@@ -15,4 +15,5 @@ def load_series(filename, series_idx = 1):
     except IOError:
         return None
 
-def split_data(data, percent_train = 0.8)
+def split_data(data, percent_train = 0.8):
+    num_ro
