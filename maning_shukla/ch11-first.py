80d18a0e1ab72f1de3b1ce63b51463cc939ebca9
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-05 00:44:48 -0400

    Updated ch11-first.py.

--- maning_shukla/ch11-first.py
+++ maning_shukla/ch11-first.py
@@ -53,4 +53,4 @@ print(lookup_0d)
 def extract_character_vocab(data):
     special_symbols = ["<PAD>", "<UNK>", "<GO>", "<EOS>"]
     set_symbols = set([character for line in data for character in line])
-    all_symbols = s
+    all_symbols = special_symbols
