e4063c5efcfc70f31dc76429203f892f4c349168
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-05 00:13:55 -0400

    Updated ch11-first.py.

--- maning_shukla/ch11-first.py
+++ maning_shukla/ch11-first.py
@@ -12,4 +12,6 @@ def make_cell(state_dim):
 
 with tf.variable_scope("first_cell") as scope:
     cell = make_cell(state_dim = 10)
-    outputs, states = tf.nn.dynamic_rnn(cell, input_placeholder, dtype = )
+    outputs, states = tf.nn.dynamic_rnn(cell, input_placeholder, dtype = tf.float32)
+
+with 
