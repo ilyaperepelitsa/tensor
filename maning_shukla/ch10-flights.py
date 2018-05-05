14ef246e470b4a7517d29895defa00d9389a9f9b
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 23:19:18 -0400

    Updated ch10-flights.py.

--- maning_shukla/ch10-flights.py
+++ maning_shukla/ch10-flights.py
@@ -77,4 +77,4 @@ predictor = SeriesPredictor(
     hidden_dim = 100)
 
 data = data_loader.load_series("/Users/ilyaperepelitsa/Downloads/international-airline-passengers.csv")
-train_data, actual_vals = data_loader.spli
+train_data, actual_vals = data_loader.split_data(data)
