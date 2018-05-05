efa590a11f418d0dfe4b2370f7520bac1bea4f75
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 15:30:11 -0400

    Updated ch3_names.py.

--- hope_reshev/ch3_names.py
+++ hope_reshev/ch3_names.py
@@ -11,11 +11,11 @@ print(c2.name)
 
 ### NAME SCOPES
 with tf.Graph().as_default():
-    c1 = tf.constant(4, dtype = tf.float64, nale = "c")
+    c1 = tf.constant(4, dtype = tf.float64, name = "c")
     with tf.name_scope("prefix_name"):
         c2 = tf.constant(4, dtype = tf.int32, name = "c")
         c3 = tf.constant(4, dtype = tf.float64, name = "c")
 
 print(c1.name)
 print(c2.name)
-print(c3.)
+print(c3.name)
