0628bec74edd3ade157691ecac09b78b476fb30a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-05 00:36:27 -0400

    Updated ch11-first.py.

--- maning_shukla/ch11-first.py
+++ maning_shukla/ch11-first.py
@@ -26,4 +26,4 @@ multi_cell = make_multi_cell(state_dim = 10, num_layers = 4)
 outputs4, states4 = tf.nn.dynamic_rnn(multi_cell, input_placeholder, dtype = tf.float32)
 
 
-embeddings_0d = tf.constant([])
+embeddings_0d = tf.constant([17, 22, 35, 51])
