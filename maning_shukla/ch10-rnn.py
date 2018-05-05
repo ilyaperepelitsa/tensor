4b7898dac556928eee333dac188c421cb3da7257
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-04 21:49:19 -0400

    Updated ch10-rnn.py.

--- maning_shukla/ch10-rnn.py
+++ maning_shukla/ch10-rnn.py
@@ -5,4 +5,4 @@ from tensorflow.contrib import rnn
 class SeriesPredictor:
     def __init__(self, input_dim, seq_size, hidden_dim = 10):
         self.input_dim = input_dim
-        se
+        self.seq
