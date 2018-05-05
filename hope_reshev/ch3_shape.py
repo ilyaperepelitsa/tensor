7076f1f24f3564abf864376bc3f0eab834670539
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 14:57:01 -0400

    Updated ch3_shape.py.

--- hope_reshev/ch3_shape.py
+++ hope_reshev/ch3_shape.py
@@ -6,8 +6,8 @@ c = tf.constant([[1, 2, 3],
 print("Python list input: {}".format(c.get_shape()))
 
 
-c = tf.constant(np.array([[1, 2, 3],
+c = tf.constant(np.array([[[1, 2, 3],
                           [4, 5, 6]],
 
                           [[1, 1, 1],
-                           [2, 2, 2]]))
+                           [2, 2, 2]]]))
