2ff44f99466cc8ce6e69eee8b95cc337768ede18
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-05 00:10:34 -0400

    Updated ch11-first.py.

--- maning_shukla/ch11-first.py
+++ maning_shukla/ch11-first.py
@@ -4,4 +4,4 @@ import tensorflow as tf
 input_dim = 1
 seq_size = 6
 
-input_placeholder = tf.placeholder(dtype = tf.float32, shape = [None, seq_size, inpyt])
+input_placeholder = tf.placeholder(dtype = tf.float32, shape = [None, seq_size, input_dim])
