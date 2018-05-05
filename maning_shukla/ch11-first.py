ff1cdc3e9608453c6794962fe305807d3b9a1ca4
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 01:01:55 -0400

    Updated ch11-first.py.

--- maning_shukla/ch11-first.py
+++ maning_shukla/ch11-first.py
@@ -62,5 +62,5 @@ def extract_character_vocab(data):
 input_setences = ["hello stranger", "bye bye"]
 output_sentences = ["hiya", "later alligator"]
 
-input_int_to_symbol, input_symbol_to_int = extract_character_vocab(input_sentences)
-output_int_to_symbol, output_symbol_to_int = extract_character_vocab(output_)
+input_int_to_symbol, input_symbol_to_int = extract_character_vocab(input_setences)
+output_int_to_symbol, output_symbol_to_int = extract_character_vocab(output_sentences)
