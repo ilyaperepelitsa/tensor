d11f13fab61d9370a14d32a6909b4c788f0309bd
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 14:55:12 -0400

    Updated ch3_shape.py.

--- hope_reshev/ch3_shape.py
+++ hope_reshev/ch3_shape.py
@@ -3,4 +3,7 @@ import tensorflow as tf
 c = tf.constant([[1, 2, 3],
                   [4, 5, 6]])
 
-print("Python list input: {}".format(c.get_shape))
+print("Python list input: {}".format(c.get_shape()))
+
+
+c = 
