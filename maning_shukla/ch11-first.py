2a9dd0d9d6c83dcc0ca941c02aa8dd8281b91fd4
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2018-05-05 00:37:05 -0400

    Updated ch11-first.py.

--- maning_shukla/ch11-first.py
+++ maning_shukla/ch11-first.py
@@ -27,4 +27,5 @@ outputs4, states4 = tf.nn.dynamic_rnn(multi_cell, input_placeholder, dtype = tf.
 
 
 embeddings_0d = tf.constant([17, 22, 35, 51])
-embeddings_4d = tf.constant([[1, 0, 0 , 0]])
+embeddings_4d = tf.constant([[1, 0, 0 , 0],
+                            ])
