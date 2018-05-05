0a1f5878d22c364e916b55fbf1346afa846983c0
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 03:25:56 -0400

    Updated ch2_intro.py.

--- maning_shukla/ch2_intro.py
+++ maning_shukla/ch2_intro.py
@@ -5,4 +5,5 @@ DATA_DIR = "/tmp/data"
 NUM_STEPS = 1000
 MINIBATCH_SIZE = 100
 
-data = input_data.read_data_sets(DATA_DIR, one_ho
+data = input_data.read_data_sets(DATA_DIR, one_hot = True)
+x = tf.placeh
