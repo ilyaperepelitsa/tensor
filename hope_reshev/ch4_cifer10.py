9cdaeab780858fdb5521f12c88b3dd039481a446
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 14:33:32 -0400

    Updated ch4_cifer10.py.

--- hope_reshev/ch4_cifer10.py
+++ hope_reshev/ch4_cifer10.py
@@ -10,4 +10,4 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 def conv2d(x, W):
-    return(tf.nn.conv2d(x, W, strides = [1,]))
+    return(tf.nn.conv2d(x, W, strides = [1, 1, 1, 1], paddi))
