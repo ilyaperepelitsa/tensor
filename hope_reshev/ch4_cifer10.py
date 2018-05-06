2af680cd3cb3c5bc32bd39b2fcd747b2e7148371
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 15:17:05 -0400

    Updated ch4_cifer10.py.

--- hope_reshev/ch4_cifer10.py
+++ hope_reshev/ch4_cifer10.py
@@ -33,5 +33,6 @@ x = tf.placeholder(tf.float32, shape = [None, 784])
 y_ = tf.placeholder(tf.float32, shape = [None, 10])
 
 x_image = tf.reshape(x, [-1, 28, 28, 1])
+
 conv1 = conv_layer(x_image, shape = [5, 5, 1, 32])
 conv1_pool = max_pool_2x2(conv1)
