b85a748f6be19c48c63497516792949ecfd62a75
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 14:34:29 -0400

    Updated ch4_cifer10.py.

--- hope_reshev/ch4_cifer10.py
+++ hope_reshev/ch4_cifer10.py
@@ -13,4 +13,4 @@ def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides = [1, 1, 1, 1], padding = "SAME")
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize = [1 ,])
+    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], )
