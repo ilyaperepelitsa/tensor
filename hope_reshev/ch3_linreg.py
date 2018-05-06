b0b2a05aa4618a82645a1a1a81fa537615b20d24
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 12:48:31 -0400

    Updated ch3_linreg.py.

--- hope_reshev/ch3_linreg.py
+++ hope_reshev/ch3_linreg.py
@@ -23,4 +23,4 @@ with g.as_default():
         b = tf.Variable(0, dtype = tf.float32, name = "bias")
         y_pred = tf.matmul(w, tf.transpose(x)) + b
 
-    with tf.name_scope("l")
+    with tf.name_scope("loss") as scope:
