43f919544b28aafa05a0389ed1b6ce75c0e6a3a1
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-05 23:50:05 -0400

    Updated ch3_feed_dict.py.

--- hope_reshev/ch3_feed_dict.py
+++ hope_reshev/ch3_feed_dict.py
@@ -15,5 +15,6 @@ with tf.Graph().as_default():
     s = tf.reduce_max(xwb)
     with tf.Session() as sess:
         outs = sess.run(s, feed_dict = {x : x_data, w : w_data})
+        pew
 
 print(outs)
