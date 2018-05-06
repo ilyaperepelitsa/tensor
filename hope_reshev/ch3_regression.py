21f49b11e37553c83116e137916912c136f46f01
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 11:36:02 -0400

    Updated ch3_regression.py.

--- hope_reshev/ch3_regression.py
+++ hope_reshev/ch3_regression.py
@@ -4,3 +4,6 @@ x = tf.placeholder(tf.float32, shape = [None, 3])
 y_true = tf.placeholder(tf.float32, shape = None)
 w = tf.Variable([[0, 0, 0]], dtype = tf.float32, name = "weights")
 b = tf.Variable(0, dtype = tf.float32, name = "bias")
+
+
+y_pre
