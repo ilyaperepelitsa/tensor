286b50816ca579d7dc97dc0cacd42d1776972f95
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 12:54:10 -0400

    Updated ch3_linreg.py.

--- hope_reshev/ch3_linreg.py
+++ hope_reshev/ch3_linreg.py
@@ -36,7 +36,7 @@ with g.as_default():
         sess.run(init)
         for step in range(NUM_STEPS):
             sess.run(train, {x: x_data, y_true: y_data})
-            if (step % 5 == 0):
+            if (step % 100 == 0):
                 print(step, sess.run([w, b]))
                 wb_.append(sess.run([w, b]))
 
