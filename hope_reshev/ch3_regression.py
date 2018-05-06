efd93f519aacf0ccdb41ab23472609f9d688c413
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 11:29:32 -0400

    Updated ch3_regression.py.

--- hope_reshev/ch3_regression.py
+++ hope_reshev/ch3_regression.py
@@ -2,4 +2,4 @@ import tensorflow as tf
 
 x = tf.placeholder(tf.float32, shape = [None, 3])
 y_true = tf.placeholder(tf.float32, shape = None)
-w = tf.Variable([[0, 0, 0]])
+w = tf.Variable([[0, 0, 0]], dtype = )
