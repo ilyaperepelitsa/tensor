654779f3c1c8b4389de939d5932c4157f796701f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 14:32:38 -0400

    Updated ch4_cifer10.py.

--- hope_reshev/ch4_cifer10.py
+++ hope_reshev/ch4_cifer10.py
@@ -5,4 +5,5 @@ def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev = 0.1)
     return tf.Variable(initial)
 
-def bias_variable(shape)
+def bias_variable(shape):
+    initial = tf
