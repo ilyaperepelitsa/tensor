6e0ad534f0eb09cc910faa0019fca8ead11143b9
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 11:46:21 -0400

    Updated ch3_regression.py.

--- hope_reshev/ch3_regression.py
+++ hope_reshev/ch3_regression.py
@@ -7,4 +7,4 @@ b = tf.Variable(0, dtype = tf.float32, name = "bias")
 
 y_pred = tf.matmul(w, tf.transpose(x)) + b
 
-loss = tf.reduce_mean(tf.square(y_true - ))
+loss = tf.reduce_mean(tf.square(y_true - y))
