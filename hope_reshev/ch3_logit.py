0983eee33f2a9315096f6825daaa21f23d57c074
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-06 13:17:23 -0400

    Updated ch3_logit.py.

--- hope_reshev/ch3_logit.py
+++ hope_reshev/ch3_logit.py
@@ -33,7 +33,7 @@ with g.as_default():
 
     with tf.name_scope("loss") as scope:
         loss = tf.reduce_mean(tf.square(y_true - y_pred))
-        loss = y_true * 
+        loss = y_true * tf.log(y)
 
     with tf.name_scope("train") as scope:
         learning_rate = 0.001
