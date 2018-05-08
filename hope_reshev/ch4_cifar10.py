4b58bc230dcb905b736a12cfd492717f66ab8eed
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 00:42:05 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -54,5 +54,5 @@ def one_hot(vec, vals = 10):
 d = CifarDataManager()
 print("Number of train images: {}".format(len.d.train(images)))
 print("Number of train labels: {}".format(len.d.train(images)))
-print("Number of train images: {}".format(len.d.train(images)))
+print("Number of test images: {}".format(len.d.train(images)))
 print("Number of train images: {}".format(len.d.train(images)))
