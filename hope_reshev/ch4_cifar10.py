04f869072a182d4ed605b44c3c589a1e0ba8f92d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 02:28:08 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import pickle
-from hope_reshev.generic import weight_variable, bias_variable, conv2d, max_pool_2x2, conv_layer
+from hope_reshev.generic import weight_variable, bias_variable, conv2d, max_pool_2x2, conv_layer, full_layer
 
 DATA_PATH = "/Users/ilyaperepelitsa/Downloads/cifar-10-batches-py/"
 
