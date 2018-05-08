581960dd70ae9d7c8719bccae31dbbb275608862
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-07 23:53:46 -0400

    Updated ch4_cifer10.py.

--- hope_reshev/ch4_cifer10.py
+++ hope_reshev/ch4_cifer10.py
@@ -73,4 +73,4 @@ with tf.Session() as sess:
                                                              y_: batch[1],
                                                              keep_prob: 1.0})
             print("step {}, train accuracy {}".format(i, train_accuracy))
-        sess.run(t)
+        sess.run(train_step, feed)
