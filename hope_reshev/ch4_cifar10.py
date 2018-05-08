f2674a7b5d9b2d4e02a6b4d968b4c19830c3f3d3
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 02:48:52 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -97,5 +97,5 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 def test(sess):
     X = cifar.test.images.reshape(10, 1000, 32, 32, 3)
     Y = cifar.test.labels.reshape(10, 1000, 10)
-    acc= np.mean([sess.run(accuracy, feed_dict = {x: X[i], y_: Y[i], keep_prob = 1.0})])
-                        for i in range(1)
+    acc= np.mean([sess.run(accuracy, feed_dict = {x: X[i], y_: Y[i], keep_prob = 1.0})
+                        for i in range(10)])
