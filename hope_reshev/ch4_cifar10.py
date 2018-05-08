7c5888e3f67b2bfd5a3aa5c9cd2fe0faefa70b12
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 02:17:04 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -71,4 +71,4 @@ cifar = CifarDataManager()
 
 x = tf.placeholder(tf.float32, shape = [None, 32, 32, 3])
 y_ = tf.placeholder(tf.float32, shape = [None, 10])
-keep_prob = tf.placeho
+keep_prob = tf.placeholder(tf.float32)
