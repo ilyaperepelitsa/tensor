857111c6d7059ee930b2bde5064c12f7e1e178fc
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 00:16:43 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -12,4 +12,4 @@ class CifarLoader(object):
         data = [unpickle(f) for f in self._souce]
         images = np.vstack([d["data"] for d in data])
         n = len(images)
-        self.images = images.reshape(n, )
+        self.images = images.reshape(n, 3, )
