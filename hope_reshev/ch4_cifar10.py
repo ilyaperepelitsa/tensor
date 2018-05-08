a309e950ddc57f563d0aa413d6f1fa797678b77f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 02:53:30 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -109,3 +109,4 @@ with tf.Session() as sess:
     for i in range(STEPS):
         batch = cifar.train.next_batch(BATCH_SIZE)
         sess.run(train_step, feed_dict = {x: batch[0], y_: batch[1], keep_prob: 0.5})
+        
