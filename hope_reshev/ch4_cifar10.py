ba13f72fea28ae61dc11e482d97d8f676694665f
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 02:38:22 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -92,4 +92,4 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = 
 train_step = tr.train.AdamOptimizer(1e-3).minimize(cross_entropy)
 
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast())
+accuracy = tf.reduce_mean(tf.cast(correct_prediction))
