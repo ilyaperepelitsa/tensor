2c754eee371633f64229eca7ea08ce7527ff7a00
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 00:39:53 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -38,7 +38,7 @@ def display_cifar(images, size):
     n = len(images)
     plt.figure()
     plt.gca().set_axis_off()
-    im = n
+    im = np.vstack
 
 DATA_PATH = "/Users/ilyaperepelitsa/Downloads/cifar-10-batches-py"
 
