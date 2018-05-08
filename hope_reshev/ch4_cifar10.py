80b53fee95ff7e58e0c4c1ae3c770b9881ec4dea
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 02:51:05 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -7,7 +7,7 @@ from hope_reshev.generic import weight_variable, bias_variable, conv2d, max_pool
 
 DATA_PATH = "/Users/ilyaperepelitsa/Downloads/cifar-10-batches-py/"
 STEPS = 1000
-
+BATCH_SIZE 
 def unpickle(file):
     with open(os.path.join(DATA_PATH, file), 'rb') as fo:
         u = pickle._Unpickler(fo)
@@ -106,4 +106,4 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for i in range(STEPS):
-        batch = cifar.train.next_batch)
+        batch = cifar.train.next_batch()
