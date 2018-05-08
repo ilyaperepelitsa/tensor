1caf55b3f74ae236e8f0f37ea6f72c6c9aaedd3a
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 03:07:25 -0400

    Updated ch4_cifar10_1.py.

--- hope_reshev/ch4_cifar10_1.py
+++ hope_reshev/ch4_cifar10_1.py
@@ -7,7 +7,7 @@ from hope_reshev.generic import weight_variable, bias_variable, conv2d, max_pool
 
 DATA_PATH = "/Users/ilyaperepelitsa/Downloads/cifar-10-batches-py/"
 STEPS = 3000
-BATCH_SIZE = 50
+BATCH_SIZE = 200
 
 def unpickle(file):
     with open(os.path.join(DATA_PATH, file), 'rb') as fo:
