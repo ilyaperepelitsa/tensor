c22b3bd8a4853e77d455e63029eeee4828b734af
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 02:49:53 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -101,4 +101,5 @@ def test(sess):
                         for i in range(10)])
     print("Accuracy: {:.4}%".format(acc * 100))
 
-with tf.Session() as se
+with tf.Session() as sess:
+    sess.run()
