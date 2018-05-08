a6456b6991f6bdebc3efca013e0a4eb312594e85
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-07 23:46:01 -0400

    Updated ch4_cifer10.py.

--- hope_reshev/ch4_cifer10.py
+++ hope_reshev/ch4_cifer10.py
@@ -57,4 +57,4 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
 
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, ))
