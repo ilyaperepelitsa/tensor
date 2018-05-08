ea578dbd17b26874e15595c28cecda0f5e9c8f04
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 03:03:48 -0400

    Updated ch4_cifar10_1.py.

--- hope_reshev/ch4_cifar10_1.py
+++ hope_reshev/ch4_cifar10_1.py
@@ -90,7 +90,7 @@ conv3_pool = max_pool_2x2(conv3)
 conv3_flat = tf.reshape(conv3_pool, [-1, 4 * 4 * 128])
 conv3_drop = tf.nn.dropout(conv3_flat, keep_prob = keep_prob)
 
-full_1 = tf.nn.relu(full_layer(conv3_drop, 1024))
+full_1 = tf.nn.relu(full_layer(conv3_drop, 512))
 full1_drop = tf.nn.dropout(full_1, keep_prob = keep_prob)
 
 y_conv = full_layer(full1_drop, 10)
