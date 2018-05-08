d3bdcd53e2bebc34a1e78c6cd2c923b040237590
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-08 00:39:09 -0400

    Updated ch4_cifar10.py.

--- hope_reshev/ch4_cifar10.py
+++ hope_reshev/ch4_cifar10.py
@@ -34,7 +34,7 @@ class CifarDataManager(object):
                         for i in range(1, 6)]).load()
         self.test = CifarLoader(["test_batch"]).load()
 
-def 
+def display_cifar
 
 DATA_PATH = "/Users/ilyaperepelitsa/Downloads/cifar-10-batches-py"
 
