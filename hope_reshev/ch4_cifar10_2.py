f7a43118d53b93b0fc2c4b595544bec9271ac83f
Ilya Perepelitsa <ilya@dataslap.com>
2018-05-08 22:04:03 -0400

    Updated ch4_cifar10_2.py.

--- hope_reshev/ch4_cifar10_2.py
+++ hope_reshev/ch4_cifar10_2.py
@@ -92,3 +92,10 @@ conv1_2 = conv_layer(conv1_1, shape = [3, 3, C1, C1])
 conv1_3 = conv_layer(conv1_2, shape = [3, 3, C1, C1])
 conv1_pool = max_pool_2x2(conv1_3)
 conv1_drop = tf.nn.dropout(conv1_pool, keep_prob = keep_prob)
+
+
+conv1_1 = conv_layer(x, shape = [3, 3, 3, C1])
+conv1_2 = conv_layer(conv1_1, shape = [3, 3, C1, C1])
+conv1_3 = conv_layer(conv1_2, shape = [3, 3, C1, C1])
+conv1_pool = max_pool_2x2(conv1_3)
+conv1_drop = tf.nn.dropout(conv1_pool, keep_prob = keep_prob)
