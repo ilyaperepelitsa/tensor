3dd5e2effdcec1280301a9d23e4d1a3363bdc9d5
Ilya Perepelitsa <ilya@dataslap.com>
2018-05-08 22:19:55 -0400

    Updated ch4_cifar10_2.py.

--- hope_reshev/ch4_cifar10_2.py
+++ hope_reshev/ch4_cifar10_2.py
@@ -112,3 +112,17 @@ full1 = tf.nn.relu(full_layer(conv3_flat, F1))
 full1_drop = tf.nn.dropout(full1, keep_prob = keep_prob)
 
 y_conv = full_layer(full1_drop, 10)
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = y_conv, labels = y_))
+
+train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+def test(sess):
+    X = cifar.test.images.reshape(10, 1000, 32, 32, 3)
+    Y = cifar.test.labels.reshape(10, 1000, 10)
+    acc= np.mean([sess.run(accuracy, feed_dict = {x: X[i], y_: Y[i], keep_prob: 1.0})
+                        for i in range(10)])
+    print("Accuracy: {:.4}%".format(acc * 100))
