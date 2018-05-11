709012f9c3a96d2b295e94bc1dca16635ce64763
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-11 02:23:38 -0400

    Updated ch5_rnn_1.py.

--- hope_reshev/ch5_rnn_1.py
+++ hope_reshev/ch5_rnn_1.py
@@ -30,4 +30,4 @@ def variable_summaries(var):
         with tf.name_scope("stddev"):
             stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
         tf.summary.scalar("stddev", stddev)
-        tf.summary.scalar("max", )
+        tf.summary.scalar("max", tf.re)
