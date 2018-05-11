ffc1bcfd0a5e1b698cf12147e9e9fa88a3004349
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-05-11 02:33:51 -0400

    Updated ch5_rnn_1.py.

--- hope_reshev/ch5_rnn_1.py
+++ hope_reshev/ch5_rnn_1.py
@@ -55,4 +55,4 @@ def rnn_step(previous_hidden_state, x):
 
 # Current input shape = (batch_size, time_steps, element_size)
 processed_input = tf.transpose(_inputs, perm = [1, 0, 2])
-# Current input shape = 
+# Current input shape = (time_step)
