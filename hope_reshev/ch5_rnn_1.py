be87f4ab2f1368587b1038e86ac9a4b0e1d0d5c2
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-06-08 22:40:53 -0400

    Updated ch5_rnn_1.py.

--- hope_reshev/ch5_rnn_1.py
+++ hope_reshev/ch5_rnn_1.py
@@ -82,4 +82,4 @@ with tf.name_scope("linear_layer_weights") as scope:
 
 
 with tf.name_scope("cross_entropy"):
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits())
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = outp))
