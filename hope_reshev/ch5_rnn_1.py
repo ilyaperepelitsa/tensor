a682298b1451d07c3af365e724a095e0aae24c91
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-06-09 10:43:54 -0400

    Updated ch5_rnn_1.py.

--- hope_reshev/ch5_rnn_1.py
+++ hope_reshev/ch5_rnn_1.py
@@ -122,4 +122,5 @@ witf tf.Session() as sess:
                                                                         y: test_label})
             test_acc = sess.run(accuracy, feed_dict={_inputs: test_data,
                                                         y: test_label})
-    test_acc = sess.run(accuracy, feed_dict = {_inputs: tes})
+    test_acc = sess.run(accuracy, feed_dict = {_inputs: test_data,
+                                                })
