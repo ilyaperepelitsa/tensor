c14f83267fa6464b57095a4c0b7bea3c9488d262
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-06-08 22:52:27 -0400

    Updated ch5_rnn_1.py.

--- hope_reshev/ch5_rnn_1.py
+++ hope_reshev/ch5_rnn_1.py
@@ -102,4 +102,4 @@ test_label = mnist.test.labels[:batch_size]
 
 witf tf.Session() as sess:
     train_writer = tf.summary.FileWriter(LOG_DIR + "/train", graph = tf.get_default_graph())
-    
+    test_writer 
