2bab696f4ddcdb8bd705f52059d76e04edc53b8e
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-06-08 22:23:10 -0400

    Updated ch5_rnn_1.py.

--- hope_reshev/ch5_rnn_1.py
+++ hope_reshev/ch5_rnn_1.py
@@ -74,4 +74,4 @@ with tf.name_scope("linear_layer_weights") as scope:
 def get_linear_layer(hidden_state):
     return tf.matmul(hidden_state)
 
-with tf.name_scope("l")
+with tf.name_scope("linear_l")
